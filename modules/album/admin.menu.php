9720634f0e2ff4f2446889ebd4c52331e1c6ce5e
daiubqn113 <daiu1998@gmail.com>
2020-12-03 16:34:46 +0700

    chuc nang album

---
+++ modules/album/admin.menu.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC <contact@vinades.vn>
+ * @Copyright (C) 2020 VINADES.,JSC. All rights reserved
+ * @License: Not free read more http://nukeviet.vn/vi/store/modules/nvtools/
+ * @Createdate Tue, 10 Nov 2020 06:56:08 GMT
+ */
+
+if (!defined('NV_ADMIN')) {
+    die('Stop!!!');
+}
+
+$submenu['config'] = $lang_module['config'];
+$submenu['album'] = $lang_module['album'];
+$submenu['add_album'] = $lang_module['add_album'];
+$submenu['album_product'] = $lang_module['album_product'];
+$submenu['add_album_product'] = $lang_module['add_album_product'];
